96a00784f275d958fa719d2ab2e6433a1d89191e
buttfa <1662332017@qq.com>
2024-07-20 12:45:25 +0800

    编写了iniSave测试单元，用于检测einip中与保存操作相关的函数 / Developed iniSave testing unit to detect functions related to save operations in einip

--- test/testIniSave.cpp
+++ test/testIniSave.cpp
@@ -2,6 +2,92 @@
 #include "ini.h"
 #include "check.h"
 
+
+TEST(testSave, saveFile_test0) {
+    // 1. 初始化测试用例
+    char* test_str = (char*)"[section1]\nkey1=value1\nkey2=value2\n";
+    char* expections[] = {(char*)"section1", 
+                            (char*)"key1", (char*)"value1",
+                            (char*)"key2", (char*)"value2"};
+
+    // 2. 创建测试用ini
+    FILE* test_stream = fmemopen(test_str, strlen(test_str), "r");
+    ini* test_ini = NULL; 
+    iniParseStat test_stat = iniParse(test_stream, (ini**)&test_ini);
+    
+    // 3. 保存和释放测试用ini
+    iniSaveFile(test_ini, "test.ini");
+    fclose(test_stream);
+    iniFree(test_ini);
+
+    // 4. 读取保存的ini
+    FILE* target_stream = fopen("test.ini", "r");
+    ini* target_ini = NULL;
+    iniParseStat target_stat = iniParse(target_stream, (ini**)&target_ini);
+
+    // 5. 检测结果
+    checkIni(target_ini, expections, sizeof(expections)/sizeof(*expections));
+    fclose(target_stream);
+    iniFree(target_ini);
+}
+
+TEST(testSave, saveFile_test1) {
+    // 1. 初始化测试用例
+    char* test_str = (char*)"[section1]\nkey1=value1\nkey2=value2\n";
+    char* expections[] = {(char*)"section1", 
+                            (char*)"key2", (char*)"value2"};
+
+    // 2. 创建测试用ini
+    FILE* test_stream = fmemopen(test_str, strlen(test_str), "r");
+    ini* test_ini = NULL; 
+    iniParseStat test_stat = iniParse(test_stream, (ini**)&test_ini);
+    iniDelKey(iniGetSection(test_ini, "section1"), "key1");
+    
+    // 3. 保存和释放测试用ini
+    iniSaveFile(test_ini, "test.ini");
+    fclose(test_stream);
+    iniFree(test_ini);
+
+    // 4. 读取保存的ini
+    FILE* target_stream = fopen("test.ini", "r");
+    ini* target_ini = NULL;
+    iniParseStat target_stat = iniParse(target_stream, (ini**)&target_ini);
+
+    // 5. 检测结果
+    checkIni(target_ini, expections, sizeof(expections)/sizeof(*expections));
+    fclose(target_stream);
+    iniFree(target_ini);
+}
+
+TEST(testSave, saveFile_test2) {
+    // 1. 初始化测试用例
+    char* test_str = (char*)"[section1]\nkey1=value1\nkey2=value2\n";
+    char* expections[] = {(char*)"section2"};
+
+    // 2. 创建测试用ini
+    FILE* test_stream = fmemopen(test_str, strlen(test_str), "r");
+    ini* test_ini = NULL; 
+    iniParseStat test_stat = iniParse(test_stream, (ini**)&test_ini);
+    iniDelKey(iniGetSection(test_ini, "section1"), "key1");
+    iniDelSection(test_ini, "section1");
+    iniAddSection(test_ini, "section2");
+    
+    // 3. 保存和释放测试用ini
+    iniSaveFile(test_ini, "test.ini");
+    fclose(test_stream);
+    iniFree(test_ini);
+
+    // 4. 读取保存的ini
+    FILE* target_stream = fopen("test.ini", "r");
+    ini* target_ini = NULL;
+    iniParseStat target_stat = iniParse(target_stream, (ini**)&target_ini);
+
+    // 5. 检测结果
+    checkIni(target_ini, expections, sizeof(expections)/sizeof(*expections));
+    fclose(target_stream);
+    iniFree(target_ini);
+}
+
 TEST(testSave, saveStr_test0) {
     // 1. 初始化测试用例
     char* test_str = (char*)"[section1]\nkey1=value1\nkey2=value2\n[section2]\nkey1=value1\nkey2=value2\n";
